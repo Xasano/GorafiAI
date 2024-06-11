8395a83eda734c3a38a4c9ba3a144945fc4f6fb9
Vasseur.Matthéo <mahtvssr@gmail.com>
2024-06-11 18:30:16 +0200

    URL + create a page for generate articles

--- GorafiAI/src/main.ts
+++ GorafiAI/src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { HomeComponent } from './app/components/home/home.component';
+import { ArticlesComponent } from './app/components/articles/articles.component';
 
-bootstrapApplication(HomeComponent, appConfig)
+bootstrapApplication(ArticlesComponent, appConfig)
   .catch((err) => console.error(err));
