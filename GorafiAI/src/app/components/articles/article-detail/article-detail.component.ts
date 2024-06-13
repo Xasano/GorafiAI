4fc477f8fbfe77ef501517ea08ce09d6b257272c
Vasseur.Matthéo <mahtvssr@gmail.com>
2024-06-13 16:23:56 +0200

    correction bug page articles

--- GorafiAI/src/app/components/articles/article-detail/article-detail.component.ts
+++ GorafiAI/src/app/components/articles/article-detail/article-detail.component.ts
@@ -1,38 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ActivatedRoute, RouterModule  } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 import { ArticleService } from '../../../services/article.service';
 import { Article } from '../../../models/article';
 
 @Component({
   selector: 'app-article-detail',
   standalone: true,
-  imports: [CommonModule, RouterModule ],
+  imports: [CommonModule, RouterModule],
   templateUrl: './article-detail.component.html',
   styleUrls: ['./article-detail.component.css']
 })
 export class ArticleDetailComponent implements OnInit {
-  articleId: string = '';
-  article: Article = {
-    id: 'defaultId',
-    titre: 'Lorsque Luffy apprend que Macron dissoud l\'assemblé',
-    resume: 'Suite à la prise de parole de Macron Luffy tira une de cest tete',
-    photo: 'https://wallpaperaccess.com/full/5666772.jpg',
-    contenu: 'Description détaillée de l\'événement.',
-    auteur: 'Nom de l\'auteur',
-    dateCreation: new Date(),  
-    mp3: 'path_to_audio_file.mp3'
-  };
+  article: Article | null = null;
 
-  constructor(private route: ActivatedRoute, private articleService: ArticleService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private articleService: ArticleService
+  ) {}
 
-  ngOnInit() {
-    this.articleId = this.route.snapshot.paramMap.get('id') || 'defaultId';
-    this.articleService.getArticleById(this.articleId).subscribe(
+  ngOnInit(): void {
+    this.route.paramMap.pipe(
+      switchMap(params => {
+        const id = params.get('id') || 'defaultId';
+        return this.articleService.getArticleById(id);
+      })
+    ).subscribe(
       article => this.article = article,
-      error => {
-        console.error('Erreur lors de la récupération de l\'article', error);
-      }
+      error => console.error('Erreur lors de la récupération de l\'article', error)
     );
   }
 }
