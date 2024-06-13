65132611b3c290fabeb2a0d553a93b12b009df1a
Vasseur.Matthéo <mahtvssr@gmail.com>
2024-06-13 23:07:21 +0200

    bug fix footer and header

--- GorafiAI/src/main.ts
+++ GorafiAI/src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { ArticlesComponent } from './app/components/articles/articles.component';
+//import { HeaderComponent } from './app/components/header/header.component';
+//import { FooterComponent } from './app/components/footer/footer.component';
 
 bootstrapApplication(ArticlesComponent, appConfig)
   .catch((err) => console.error(err));
