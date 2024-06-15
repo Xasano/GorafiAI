462a78383238a0559c6da6dc28954d61732fa8ae
Lefumeux Bastien <bastien.lefumeux@etu.unice.fr>
2024-06-15 20:05:39 +0200

    modified:   GorafiAI/src/app/components/generate-articles/generate-articles.component.html 	modified:   GorafiAI/src/app/components/generate-articles/generate-articles.component.ts

--- GorafiAI/src/app/components/generate-articles/generate-articles.component.ts
+++ GorafiAI/src/app/components/generate-articles/generate-articles.component.ts
@@ -2,7 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule, Router } from '@angular/router';
 import { ArticleService } from '../../services/article.service';
-import { FormsModule } from '@angular/forms';
+import { NgForm,FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-generate-articles',
@@ -13,11 +14,14 @@ import { FormsModule } from '@angular/forms';
 })
 export class GenerateArticlesComponent {
   article: any;
+  title?: string;
+  author?: string;
   
-  constructor(private articleService: ArticleService, private router: Router) {
-  } 
-  generateArticle(titre: string, auteur: string): void {
-    this.articleService.generateArticle(titre, auteur).subscribe({
+  constructor(private articleService: ArticleService, private router: Router) {}
+
+  generateArticle(form :NgForm): void {
+    console.log('Génération de l\'article', form.value);
+    this.articleService.generateArticle(form.value.title, form.value.author).subscribe({
       next: (article) => {
       // Redirect the user to the generated article page
       this.router.navigate(['/article', article._id]);
