4f1fb3b28cd12029f78b6627c227b1cbc29df93c
Maht83 <mattheovasseur6@gmail.com>
2024-06-15 16:51:21 +0200

    routage bug fix

--- GorafiAI/src/main.ts
+++ GorafiAI/src/main.ts
@@ -1,16 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { ArticlesComponent } from './app/components/articles/articles.component';
-
 import { provideZoneChangeDetection} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { AppComponent } from './app/app.component';
 
-bootstrapApplication(ArticlesComponent, {
+bootstrapApplication(AppComponent, {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
-    provideHttpClient(withFetch())]
+    provideHttpClient(withFetch())
+  ]
 })
   .catch((err) => console.error(err));
-
